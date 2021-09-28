82f47670d240b663279a7d3b37642ec60b1d9108
Joan <joanetap@gmail.com>
2021-09-28 19:44:08 +0200

    Include JSON file data

---
+++ static/data.js
@@ -0,0 +1,42 @@
+let data = 
+{
+    about: {
+        "name": "Joan name fake",
+        "phone": "666 777 888",
+        "email": "email@emailprov.com"
+    },
+    experience: [
+        {
+            "name": "Helloyalty",
+            "startDate": "01/2009",
+            "endDate": "06/2011",
+            "role" : "Fullstack Developer"
+        },
+        {
+            "name": "Duplex",
+            "startDate": "07/2011",
+            "endDate": "08/2013",
+            "role" : "Fullstack Developer"
+        }
+
+    ],
+    studies: [
+        {
+            "name" : "ESO",
+            "school" : "IES One",
+            "startDate": "09-2005",
+            "endDate": "06-2007"
+        },
+        {
+            "name" : "CFGS",
+            "school" : "IES Two",
+            "startDate": "09-2008",
+            "endDate": "06-2010"
+        }
+    ],
+    interests: 
+    [
+        'skateboarding', 'apiculture', 'guitar riffs'
+    ]
+};
+export default data;
