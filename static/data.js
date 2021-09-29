2a4be0c34a9b2bad2eb27a85eb12c94f44285a89
Joan <joanetap@gmail.com>
2021-09-29 17:52:16 +0200

    Add text to experience data object

--- static/data.js
+++ static/data.js
@@ -10,15 +10,16 @@ let data =
             "name": "Helloyalty",
             "startDate": "01/2009",
             "endDate": "06/2011",
-            "role" : "Fullstack Developer"
+            "role" : "Fullstack Developer",
+            "text" : "Skate ipsum dolor sit amet, g-turn John Carbis impossible regular footed carve. Bearings tailslide frontside air bone air. Transfer vert Video Days slob air airwalk. Ho-ho full-cab gap axle. Disaster frigid air snake slam. Crailtap slappy blunt transfer. Front foot impossible bruised heel lip quarter pipe. Bank Jeremy Klein lip switch boned out. Alien Workshop hanger feeble locals opposite footed. Slide full-cab front foot impossible Claus Grabke casper."
         },
         {
             "name": "Duplex",
             "startDate": "07/2011",
             "endDate": "08/2013",
-            "role" : "Fullstack Developer"
+            "role" : "Fullstack Developer",
+            "text" : "Mini ramp fakie freestyle air Jason Lee cab flip crail grab 540. Rocket air frontside air crail grab berm Saran Wrap lipslide Kevin Ancell hardware. Acid drop airwalk tuna-flip 900 gap hard flip sketchy. Mini ramp airwalk half-flip sketchy 1080 Japan air bluntslide. Skate or die kickflip powerslide ollie north mute-air Colby Carter grab late. Primo slide berm flail slob air rocket air poseur acid drop McTwist. Skater 540 nose acid drop indy grab frontside air speed wobbles. Camel back smith grind pivot mute-air 900 feeble regular footed. Crailtap pump frontside stalefish face plant gnar bucket hardware."
         }
-
     ],
     studies: [
         {
