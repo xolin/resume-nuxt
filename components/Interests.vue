c6509912ada733e16ce018416d77fc4c2ffa8af8
Joan <joanetap@gmail.com>
2021-09-29 17:51:39 +0200

    Add styles to interests component

--- components/Interests.vue
+++ components/Interests.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <p>{{ $t('interests.title') }}</p>
+    <div class="mt-4 w-1/3 inline">
+        <h2 class="uppercase text-xl font-light pb-4">{{ $t('interests.title') }}</h2>
         <ul>
             <li v-for="interest in interests" :key="interest.name">  
-                {{ interest }}
+                <p class="font-light text-sm">{{ interest }}</p>
             </li>
         </ul>
     </div>
