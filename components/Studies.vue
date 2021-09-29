291badda8fed6c675e050b82434e30fbb63f8098
Joan <joanetap@gmail.com>
2021-09-29 17:53:04 +0200

    Add styles to studies

--- components/Studies.vue
+++ components/Studies.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
-        <p>{{ $t('studies.title') }}</p>
-        <p v-for="study in studies" :key="study.name">  
-            {{ study.name }} : {{ study.startDate }} - {{ study.endDate }} ( {{ study.school }} )
-        </p>
+    <div class="mt-4 w-1/3 inline">
+        <h2 class="uppercase text-xl font-light pb-4">{{ $t('studies.title') }}</h2>
+        <div class="mb-5" v-for="study in studies" :key="study.name"> 
+            <p class="font-bold text-lg mb-1">{{ study.name }}</p>
+            <p class="font-light text-xs"> {{ study.school }} </p>
+            <p class="font-light text-xs"> {{ study.startDate }} - {{ study.endDate }}</p> 
+        </div>
     </div>
 </template>
 <script>
