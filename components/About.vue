9607314170ec32df0f330e933cd2721432f01fe7
Joan <joanetap@gmail.com>
2021-09-30 13:37:50 +0200

    Add github to data file and move about data block

--- components/About.vue
+++ components/About.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="container flex flex-wrap bottom-border h-40 content-center">
-        <div class="w-1/2 inline">
+        <div class="w-3/4 inline">
             <h1 class="font-bold text-2xl"> {{ info.name }} </h1>
         </div>
-        <div class="w-1/2 inline">
-            <p><a :href="'mailto:'+info.email+'?subject=Contact'"><span class="link link-underline link-underline-black text-black font-bold"> {{ info.email }} </span></a></p>
-            <p><a :href="'callto:'+info.phone"><span class="link link-underline link-underline-black text-black font-bold"> {{ info.phone }} </span></a></p>
-            <p><a href="http://github.com"><span class="link link-underline link-underline-black text-black font-bold">Github</span></a></p>
+        <div class="w-1/4 inline">
+            <p><a :href="'mailto:'+info.email+'?subject=Contact'"><span class="link link-underline link-underline-black text-black "> {{ info.email }} </span></a></p>
+            <p><a :href="'callto:'+info.phone"><span class="link link-underline link-underline-black text-black"> {{ info.phone }} </span></a></p>
+            <p><a :href="'http://github.com/'+info.github"><span class="link link-underline link-underline-black text-black">Github</span></a></p>
         </div>
     </div>
 </template>
